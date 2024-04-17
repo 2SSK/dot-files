dadf8d0a93f2f75fd315baae02f5fa2e1b1b2538
SauravSinghKarmwar02 <100ravsinghkarmwar@gmail.com>
2024-04-17 13:48:58 +0530

    Dot files / nvim cofig files

---
+++ lua/saurav/core/keymaps.lua
@@ -0,0 +1,24 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap   -- for conciseness
+
+keymap.set("n", "<leader>pv", vim.cmd.Ex)
+keymap.set("i", "jk", "<ESC>", {desc = "Exit insert mode with jk"})
+
+keymap.set("n", "<leader>nh", ":nohl<CR>", {desc = "Clear search highlights"})
+
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>", {desc = "Increment number"})  -- increment
+keymap.set("n", "<leader>-", "<C-x>", {desc = "Decrement number"})  -- Decrement
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", {desc = "Split window vertically"})
+keymap.set("n", "<leader>sh", "<C-w>s", {desc = "Split window horizontally"})
+keymap.set("n", "<leader>se", "<C-w>=", {desc = "Make splits equal size"})
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", {desc = "Close current split"})
+
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", {desc = "Open new tab"})
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", {desc = "Close current tab"})
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", {desc = "Go to next tab"})
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", {desc = "Go to previous tab"})
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", {desc = "Open current buffer in new tab"})
