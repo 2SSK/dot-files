58a19e0972ed1035599ae5aed4df68a46ffb8c7b
SauravSinghKarmwar02 <100ravsinghkarmwar@gmail.com>
2024-05-16 09:28:40 +0530

    feat: Added oil plugin and terminal keymap in nvim config

--- nvim/lua/saurav/plugins/copilot.lua
+++ nvim/lua/saurav/plugins/copilot.lua
@@ -1,24 +1,3 @@
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    enabled = true,
-    event = { "BufEnter" },
-    config = function()
-      require("copilot").setup({
-        suggestion = {
-          enabled = false,
-        },
-        panel = { enabled = false },
-      })
-    end,
-  },
-  {
-    "zbirenbaum/copilot-cmp",
-    enabled = true,
-    event = { "BufEnter" },
-    dependencies = { "zbirenbaum/copilot.lua" },
-    config = function()
-      require("copilot_cmp").setup()
-    end,
-  },
+  "github/copilot.vim",
 }
