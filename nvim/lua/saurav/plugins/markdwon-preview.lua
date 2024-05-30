9c439a38aca2fb876b2539e55e11a9e04fef6ca3
2SSK <100ravsinghkarmwar@gmail.com>
2024-05-30 09:16:27 +0530

    Update nvim banner in alpha.lua | Updated markdown plugin

--- nvim/lua/saurav/plugins/markdwon-preview.lua
+++ nvim/lua/saurav/plugins/markdwon-preview.lua
@@ -1,8 +1,9 @@
 return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  ft = { "markdown" },
-  build = function()
-    vim.fn["mkdp#util#install"]()
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
   end,
+  ft = { "markdown" },
 }
