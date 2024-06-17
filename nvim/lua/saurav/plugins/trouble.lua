a90e4d2d54df2b0df821a92d618045b8d37d5e31
SSK <100ravsinghkarmwar@gmail.com>
2024-06-17 23:09:50 +0530

    feat: Add and configure development environment settings
    - Add .nanorc with the following configurations:
      - Enable syntax highlighting
      - Display line numbers
      - Enable soft wrapping
      - Show whitespace characters
      - Set tab size to 2
      - Convert tabs to spaces
      - Enable smooth scrolling
      - Enable persistent search highlighting
      - Backup files before saving
    
    - Add .vimrc with the following settings:
      - Set space as the leader key
      - Enable syntax highlighting
      - Display line numbers and relative line numbers
      - Configure tabs and indentation
      - Enable line wrapping
      - Configure search settings
      - Enhance interface settings
      - Enable clipboard and mouse support
      - Add custom key mappings
    
    - Add tmux plugins as submodules:
      - tmux
      - tmux-continuum
      - tmux-resurrect
      - tpm
      - vim-tmux-navigator
    
    - Remove i3 configuration files:
      - i3/config
      - i3/evening-sky.png
    
    - Update nvim/lazy-lock.json with latest plugin commits:
      - Comment.nvim
      - conform.nvim
      - copilot.vim
      - friendly-snippets
      - gitsigns.nvim
      - go.nvim
      - lazy.nvim
      - mason-lspconfig.nvim
      - mason-nvim-dap.nvim
      - nvim-cmp

--- nvim/lua/saurav/plugins/trouble.lua
+++ nvim/lua/saurav/plugins/trouble.lua
@@ -1,12 +1,37 @@
 return {
   "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
   keys = {
-    { "<leader>xx", "<cmd>TroubleToggle<CR>", desc = "Open/close trouble list" },
-    { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
-    { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
-    { "<leader>xq", "<cmd>TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list" },
-    { "<leader>xl", "<cmd>TroubleToggle loclist<CR>", desc = "Open trouble location list" },
-    { "<leader>xt", "<cmd>TodoTrouble<CR>", desc = "Open todos in trouble" },
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
   },
 }
