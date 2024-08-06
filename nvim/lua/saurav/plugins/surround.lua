1c2a8f0336350d9662e33111b793ab2b3c11f8a0
SSK <100ravsinghkarmwar@gmail.com>
2024-08-06 13:18:35 +0530

    feat(nvim): Add markview.lua plaugin

--- nvim/lua/saurav/plugins/surround.lua
+++ nvim/lua/saurav/plugins/surround.lua
@@ -9,8 +9,8 @@ return {
 -- --------------------------------------------------------------------------------
 --     surr*ound_words             ysiw)           (surround_words)
 --     *make strings               ys$"            "make strings"
---     [delete ar*ound me!]        ds]             delete around me!
+--     delete ar*ound me!        ds]             delete around me!
 --     remove <b>HTML t*ags</b>    dst             remove HTML tags
 --     "change quot*es"            cs'"            "change quotes"
 --     <h1>or tag* types</h1>        csth1<CR>       <h1>or tag types</h1>
---     delete(functi*on calls)     dsf             function calls
+--     functi*on calls     dsf             function calls
