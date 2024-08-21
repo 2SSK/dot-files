2c061df1d7fa7de68df1f3748fa868f94c51265d
SSK <100ravsinghkarmwar@gmail.com>
2024-08-21 10:34:11 +0530

    powermenu in rofi neofetch and picom

--- rofi/powermenu.sh
+++ rofi/powermenu.sh
@@ -15,76 +15,76 @@ logout='󰍃'
 
 # Rofi CMD
 rofi_cmd() {
-	rofi -dmenu \
-		-p "" \
-		-mesg "Uptime: $uptime" \
-		-theme "$HOME/.config/rofi/themes/powermenu.rasi"
+    rofi -dmenu \
+        -p "" \
+        -mesg "Uptime: $uptime" \
+        -theme "$HOME/.config/rofi/themes/powermenu.rasi"
 }
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+    echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
 }
 
 # Execute Command
 run_cmd() {
-	case $1 in
-		--shutdown)
-			systemctl poweroff
-			;;
-		--reboot)
-			systemctl reboot
-			;;
-		--suspend)
-			mpc -q pause
-			amixer set Master mute
-			systemctl suspend
-			;;
-		--logout)
-			case "$DESKTOP_SESSION" in
-				openbox)
-					openbox --exit
-					;;
-				bspwm)
-					bspc quit
-					;;
-				dwm)
-					pkill dwm
-					;;
-				i3)
-					i3-msg exit
-					;;
-				plasma)
-					qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-					;;
-			esac
-			;;
-	esac
+    case $1 in
+        --shutdown)
+            systemctl poweroff
+            ;;
+        --reboot)
+            systemctl reboot
+            ;;
+        --suspend)
+            mpc -q pause
+            amixer set Master mute
+            systemctl suspend
+            ;;
+        --logout)
+            case "$DESKTOP_SESSION" in
+                openbox)
+                    openbox --exit
+                    ;;
+                bspwm)
+                    bspc quit
+                    ;;
+                dwm)
+                    pkill dwm
+                    ;;
+                i3)
+                    i3-msg exit
+                    ;;
+                plasma)
+                    qdbus org.kde.ksmserver /KSMServer logout 0 0 0
+                    ;;
+            esac
+            ;;
+    esac
 }
 
 # Actions
 chosen="$(run_rofi)"
 case "${chosen}" in
     "${shutdown}")
-		run_cmd --shutdown
+        run_cmd --shutdown
         ;;
     "${reboot}")
-		run_cmd --reboot
+        run_cmd --reboot
         ;;
     "${lock}")
-      if [[ -x '/usr/.config/i3/lockscreen.sh' ]];
-      then
-        /usr/.config/i3/lockscreen.sh
-		if [[ -x '/usr/bin/betterlockscreen' ]]; then
-			betterlockscreen -l
-		elif [[ -x '/usr/bin/i3lock' ]]; then
-			i3lock
-		fi
+        if [[ -x "$HOME/.config/i3/lockscreen.sh" ]]; then
+            "$HOME/.config/i3/lockscreen.sh"
+        fi
+        # if [[ -x '/usr/bin/betterlockscreen' ]]; then
+        #     betterlockscreen -l
+        # elif [[ -x '/usr/bin/i3lock' ]]; then
+        #     i3lock
+        # fi
         ;;
     "${suspend}")
-		run_cmd --suspend
+        run_cmd --suspend
         ;;
     "${logout}")
-		run_cmd --logout
+        run_cmd --logout
         ;;
 esac
