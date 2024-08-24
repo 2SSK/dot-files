22db59f02bb6f5949cff77da66baff64a1438312
SSK <100ravsinghkarmwar@gmail.com>
2024-08-24 15:07:47 +0530

    add xborder to i3

--- nvim/lua/ssk/plugins/colorizer.lua
+++ nvim/lua/ssk/plugins/colorizer.lua
@@ -1,8 +1,34 @@
 return {
-  "NvChad/nvim-colorizer.lua",
-  opts = {
-    user_deafult_options = {
-      tailwind = true,
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tailwindcss = {},
+      },
     },
+  },
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {
+        tailwind = true,
+      },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+    },
+    opts = function(_, opts)
+      -- original LazyVim kind icon formatter
+      if opts.formatting and opts.formatting.format then
+        local format_kinds = opts.formatting.format
+        opts.formatting.format = function(entry, item)
+          format_kinds(entry, item) -- add icons
+          return require("tailwindcss-colorizer-cmp").formatter(entry, item)
+        end
+      end
+    end,
   },
 }
