1e0cc65680eb0e4ef3a4ff6ec0de663d75c4217e
SSK <100ravsinghkarmwar@gmail.com>
2024-09-08 21:43:13 +0530

    added feature to execute file in nvim, added surround plugin

--- nvim/lua/ssk/core/keymaps.lua
+++ nvim/lua/ssk/core/keymaps.lua
@@ -21,3 +21,54 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- Execute programs from nvim
+vim.api.nvim_set_keymap("n", "<C-r>", "", {
+  noremap = true,
+  silent = true,
+  callback = function()
+    local file_path = vim.api.nvim_buf_get_name(0)
+    local file_name = vim.fn.fnamemodify(file_path, ":t")
+    local file_type = vim.bo.filetype
+
+    if file_type == "lua" then
+      vim.cmd(":terminal lua " .. file_path)
+    elseif file_type == "c" then
+      vim.cmd(
+        ":terminal gcc " .. file_path .. " -o " .. file_name:gsub("%.c$", "") .. " && ./" .. file_name:gsub("%.c$", "")
+      )
+    elseif file_type == "cpp" then
+      vim.cmd(
+        ":terminal g++ "
+          .. file_path
+          .. " -o "
+          .. file_name:gsub("%.cpp$", "")
+          .. " && ./"
+          .. file_name:gsub("%.cpp$", "")
+      )
+    elseif file_type == "python" then
+      vim.cmd(":terminal python3 " .. file_path)
+    elseif file_type == "go" then
+      vim.cmd(":terminal go run " .. file_path)
+    elseif file_type == "rust" then
+      vim.cmd(
+        ":terminal rustc "
+          .. file_path
+          .. " -o "
+          .. file_name:gsub("%.rs$", "")
+          .. " && ./"
+          .. file_name:gsub("%.rs$", "")
+      )
+    elseif file_type == "java" then
+      vim.cmd(":terminal javac " .. file_path .. " && java " .. file_name:gsub("%.java$", ""))
+    elseif file_type == "javascript" then
+      vim.cmd(":terminal node " .. file_path)
+    elseif file_type == "typescript" then
+      vim.cmd(":terminal ts-node " .. file_path)
+    elseif file_type == "sh" then
+      vim.cmd(":terminal bash " .. file_path)
+    else
+      print("File type not supported for this command")
+    end
+  end,
+})
