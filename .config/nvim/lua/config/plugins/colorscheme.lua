6d13172e6d95575fddf3c8b2f0a669f78c6b3033
2SSK <100ravsinghkarmwar@gmail.com>
2025-06-01 10:24:29 +0530

    feat: ✨ Added Gruvbox colorscheme to neovim and i3status to sway

--- .config/nvim/lua/config/plugins/colorscheme.lua
+++ .config/nvim/lua/config/plugins/colorscheme.lua
@@ -1,47 +1,69 @@
 return {
-	"folke/tokyonight.nvim",
-	priority = 1000,
-	config = function()
-		local transparent = true -- set to true if you would like to enable transparency
+	-- Gruvbox theme configuration
+	{
+		"f4z3r/gruvbox-material.nvim",
+		name = "gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			transparent = true, -- This is a plugin-specific option, but it may not work for all cases
+		},
+		config = function()
+			vim.cmd([[colorscheme gruvbox-material]])
+			-- Transparency settings
+			vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", ctermbg = "NONE" })
+			vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE", ctermbg = "NONE" })
+			vim.api.nvim_set_hl(0, "LineNr", { bg = "NONE", ctermbg = "NONE" })
+			vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE", ctermbg = "NONE" })
+		end,
+	},
+	-- Tokyonight theme configuration
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+		config = function()
+			local transparent = true -- set to true if you would like to enable transparency
 
-		local bg = "#011628"
-		local bg_dark = "#011423"
-		local bg_highlight = "#143652"
-		local bg_search = "#0A64AC"
-		local bg_visual = "#275378"
-		local fg = "#CBE0F0"
-		local fg_dark = "#B4D0E9"
-		local fg_gutter = "#627E97"
-		local border = "#547998"
-		local comment_fg = "#79a3a5"
+			local bg = "#011628"
+			local bg_dark = "#011423"
+			local bg_highlight = "#143652"
+			local bg_search = "#0A64AC"
+			local bg_visual = "#275378"
+			local fg = "#CBE0F0"
+			local fg_dark = "#B4D0E9"
+			local fg_gutter = "#627E97"
+			local border = "#547998"
+			local comment_fg = "#79a3a5"
 
-		require("tokyonight").setup({
-			style = "night",
-			transparent = transparent,
-			styles = {
-				sidebars = transparent and "transparent" or "dark",
-				floats = transparent and "transparent" or "dark",
-			},
-			on_colors = function(colors)
-				colors.bg = bg
-				colors.bg_dark = transparent and colors.none or bg_dark
-				colors.bg_float = transparent and colors.none or bg_dark
-				colors.bg_highlight = bg_highlight
-				colors.bg_popup = bg_dark
-				colors.bg_search = bg_search
-				colors.bg_sidebar = transparent and colors.none or bg_dark
-				colors.bg_statusline = transparent and colors.none or bg_dark
-				colors.bg_visual = bg_visual
-				colors.border = border
-				colors.fg = fg
-				colors.fg_dark = fg_dark
-				colors.fg_float = fg
-				colors.fg_gutter = fg_gutter
-				colors.fg_sidebar = fg_dark
-				colors.comment = comment_fg
-			end,
-		})
+			require("tokyonight").setup({
+				style = "night",
+				transparent = transparent,
+				styles = {
+					sidebars = transparent and "transparent" or "dark",
+					floats = transparent and "transparent" or "dark",
+				},
+				on_colors = function(colors)
+					colors.bg = bg
+					colors.bg_dark = transparent and colors.none or bg_dark
+					colors.bg_float = transparent and colors.none or bg_dark
+					colors.bg_highlight = bg_highlight
+					colors.bg_popup = bg_dark
+					colors.bg_search = bg_search
+					colors.bg_sidebar = transparent and colors.none or bg_dark
+					colors.bg_statusline = transparent and colors.none or bg_dark
+					colors.bg_visual = bg_visual
+					colors.border = border
+					colors.fg = fg
+					colors.fg_dark = fg_dark
+					colors.fg_float = fg
+					colors.fg_gutter = fg_gutter
+					colors.fg_sidebar = fg_dark
+					colors.comment = comment_fg
+				end,
+			})
 
-		vim.cmd("colorscheme tokyonight")
-	end,
+			vim.cmd("colorscheme tokyonight")
+		end,
+	},
 }
