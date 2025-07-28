d2f3cfb6d00e29cd312d17f3b78695f3875d7082
2SSK <100ravsinghkarmwar@gmail.com>
2025-07-28 05:48:39 +0530

    nvim config tweak

---
+++ .config/nvim/lua/config/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+	"folke/tokyonight.nvim",
+	name = "tokyonight",
+	priority = 1000,
+	config = function()
+		local comment_fg = "#79a3a5"
+		require("tokyonight").setup({
+			style = "night",
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+			on_colors = function(colors)
+				colors.comment = comment_fg
+			end,
+		})
+		vim.cmd([[colorscheme tokyonight]])
+	end,
+}
