0421d091f8038da10af669e84cb9869ec9e95cd3
2SSK <100ravsinghkarmwar@gmail.com>
2025-08-16 21:10:56 +0530

    feat: switch to nordic.nvim colorscheme, update UI configs for nord theme
    - Replace nord.nvim with nordic.nvim in Neovim config and lockfile
    - Update colorscheme setup to use nordic.nvim
    - Change Kitty, Waybar, Wlogout, and Sway configs to use nord-inspired colors and settings
    - Move Waybar to bottom, tweak modules and workspace settings
    - Adjust Sway output scale for eDP-1
    - Update Wlogout button styles for nord palette

--- .config/nvim/lua/config/plugins/colorscheme.lua
+++ .config/nvim/lua/config/plugins/colorscheme.lua
@@ -1,41 +1,12 @@
 return {
-	"shaunsingh/nord.nvim",
+	"AlexvZyl/nordic.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		-- Example config in lua
-		vim.g.nord_contrast = true
-		vim.g.nord_borders = false
-		vim.g.nord_disable_background = true
-		vim.g.set_cursorline_transparent = false
-		vim.g.nord_italic = false
-		vim.g.nord_enable_sidebar_background = false
-		vim.g.nord_uniform_diff_background = true
-		vim.g.nord_bold = false -- enables/disables bold
-
-		-- Load the colorscheme
-		require("nord").set()
-
-		-- Function to set menu borders to transparent
-		local set_menu_border_transparency = function()
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", fg = "NONE" })
-			vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE", fg = "NONE" })
-		end
-
-		-- Execute the function once after loading the colorscheme
-		set_menu_border_transparency()
-
-		local bg_transparent = true
-
-		-- Toggle background transparency
-		local toggle_transparency = function()
-			bg_transparent = not bg_transparent
-			vim.g.nord_disable_background = bg_transparent
-			vim.cmd([[colorscheme nord]])
-			-- set_menu_border_transparency()
-		end
-
-		vim.keymap.set("n", "<leader>bg", toggle_transparency, { noremap = true, silent = true })
+		require("nordic").load()
+		-- require("nordic").setup({
+		-- 	transparent = true,
+		-- })
 	end,
 }
 
