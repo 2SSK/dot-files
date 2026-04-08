7b771e1b815856ac6580048e970b58e928108cb8
2SSK <100ravsinghkarmwar@gmail.com>
2026-04-08 17:54:45 +0530

    refactor: restructure nvim config to support lazy.nvim and native pack alternatives
    - reorganize nvim/ to nvim.lazy/ for lazy.nvim setup
    - create nvim.pack/ for native pack approach
    - add treesitter.lua with commit pin and lua highlighting fix
    - update lazy-lock.json with plugin commit updates
    - reorganize plugin configs across both layouts
    - add multiplugin.lua and new plugin configs to nvim.pack

--- .config/nvim.pack/lua/config/plugins/colorscheme.lua
+++ .config/nvim.pack/lua/config/plugins/colorscheme.lua
@@ -2,16 +2,22 @@ local bg_dark = "#011423"
 local comment_fg = "#79a3a5"
 
 require("tokyonight").setup({
-  style = "night",
-  transparent = true,
-  styles = {
-    sidebars = "transparent",
-    floats = "transparent",
-  },
-  on_colors = function(colors)
-    colors.comment = comment_fg
-    colors.bg = bg_dark
-  end,
+	style = "night",
+	transparent = true,
+	styles = {
+		sidebars = "transparent",
+		floats = "transparent",
+	},
+	on_colors = function(colors)
+		colors.comment = comment_fg
+		colors.bg = bg_dark
+	end,
 })
 
 vim.cmd([[colorscheme tokyonight]])
+
+-- Apply harpoon highlight groups after colorscheme is loaded
+-- These match tokyonight's styling
+vim.api.nvim_set_hl(0, "HarpoonWindow", { link = "NormalNC" })
+vim.api.nvim_set_hl(0, "HarpoonBorder", { link = "Border" })
+vim.api.nvim_set_hl(0, "HarpoonCurrentFile", { link = "Visual" })
--- .config/nvim.pack/lua/config/plugins/harpoon.lua
+++ .config/nvim.pack/lua/config/plugins/harpoon.lua
@@ -1,24 +1,26 @@
 local harpoon = require("harpoon")
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
 
 harpoon.setup({
-  menu = {
-    width = vim.api.nvim_win_get_width(0) - 4,
-  },
-  settings = {
-    save_on_toggle = true,
-  },
+	menu = {
+		width = vim.api.nvim_win_get_width(0) - 4,
+	},
+	settings = {
+		save_on_toggle = true,
+	},
 })
 
 vim.keymap.set("n", "<leader>H", function()
-  harpoon:list():add()
+	mark.add_file()
 end, { desc = "Harpoon File" })
 
 vim.keymap.set("n", "<leader>h", function()
-  harpoon.ui:toggle_quick_menu(harpoon:list())
+	ui.toggle_quick_menu()
 end, { desc = "Harpoon Quick Menu" })
 
 for i = 1, 5 do
-  vim.keymap.set("n", "<leader>" .. i, function()
-    harpoon:list():select(i)
-  end, { desc = "Harpoon to File " .. i })
+	vim.keymap.set("n", "<leader>" .. i, function()
+		ui.nav_file(i)
+	end, { desc = "Harpoon to File " .. i })
 end
